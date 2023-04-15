74ea0d63a0a7e9c1c2366167c9fc2ea699b67e4c
suriya-prakash-s <suriyaprakashs.official@gmail.com>
2023-04-15 16:50:30 +0530

    created entity class for Employee

---
+++ src/main/java/com/noobied/erp/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.noobied.erp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false)
+    private String email;
+
+}
